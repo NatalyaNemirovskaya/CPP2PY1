37f1693006e7d75b77a19bb66150b179906abea3
NatalyaNemirovskaya <167192206+NatalyaNemirovskaya@users.noreply.github.com>
2024-12-11 19:36:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <pybind11/pybind11.h>
 
 
-int main(){
-    return 0;
+int add(int x, int y)
+{
+    return x + y;
+}
+
+namespace py = pybind11;
+
+
+
+PYBIND11_MODULE(cpp2py, m)
+{
+    m.doc() = R"pbdoc(
+        Pybind11 test
+        -------------
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(add two numbers)pbdoc");
 }
